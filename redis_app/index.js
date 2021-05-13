254d600493dce5f2ff82e8d53299d3cdeebebdd1
krohak <rohaksinghal14@gmail.com>
2021-05-14 00:11:14 +0800

    sortedsets

--- redis_app/index.js
+++ redis_app/index.js
@@ -3,6 +3,7 @@ import Strings from './strings';
 import Hashes from './hashes';
 import Lists from './Lists';
 import Sets from './sets';
+import SortedSets from './sortedSets';
 
 const redis = new Redis({ password: 'foobared123'});
 
@@ -10,3 +11,4 @@ Strings(redis);
 Hashes(redis);
 Lists(redis);
 Sets(redis);
+SortedSets(redis);
