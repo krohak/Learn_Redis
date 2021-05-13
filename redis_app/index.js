6aee0050b153d4b8b4a5779ee5e479e35e782a44
krohak <rohaksinghal14@gmail.com>
2021-05-14 00:02:24 +0800

    sets and list

--- redis_app/index.js
+++ redis_app/index.js
@@ -1,9 +1,12 @@
 import Redis from 'ioredis';
 import Strings from './strings';
 import Hashes from './hashes';
+import Lists from './Lists';
+import Sets from './sets';
 
 const redis = new Redis({ password: 'foobared123'});
 
 Strings(redis);
 Hashes(redis);
-
+Lists(redis);
+Sets(redis);
